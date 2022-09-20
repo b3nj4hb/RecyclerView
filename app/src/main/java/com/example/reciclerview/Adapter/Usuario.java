f1bfb7a12148aef497f3b6a95aa3e9c15a20ef5e
b3nj4hb <benjalive12@gmail.com>
2022-09-19 22:04:58 -0500

    Initial commit

---
+++ app/src/main/java/com/example/reciclerview/Adapter/Usuario.java
@@ -0,0 +1,72 @@
+package com.example.reciclerview.Adapter;
+
+public class Usuario {
+    private int id;
+    private String nombre;
+    private String apellidoPaterno;
+    private String apellidoMaterno;
+    private String email;
+
+    public Usuario(int id, String nombre, String apellidoPaterno, String apellidoMaterno, String email) {
+        this.id = id;
+        this.nombre = nombre;
+        this.apellidoPaterno = apellidoPaterno;
+        this.apellidoMaterno = apellidoMaterno;
+        this.email = email;
+    }
+
+    public Usuario(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getApellidoPaterno() {
+        return apellidoPaterno;
+    }
+
+    public void setApellidoPaterno(String apellidoPaterno) {
+        this.apellidoPaterno = apellidoPaterno;
+    }
+
+    public String getApellidoMaterno() {
+        return apellidoMaterno;
+    }
+
+    public void setApellidoMaterno(String apellidoMaterno) {
+        this.apellidoMaterno = apellidoMaterno;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Usuario{" +
+                "id=" + id +
+                ", nombre='" + nombre + '\'' +
+                ", apellidoPaterno='" + apellidoPaterno + '\'' +
+                ", apellidoMaterno='" + apellidoMaterno + '\'' +
+                ", email='" + email + '\'' +
+                '}';
+    }
+}
---
+++ app/src/main/java/com/example/reciclerview/Adapter/UsuarioAdaptador.java
@@ -0,0 +1,15 @@
+package com.example.reciclerview.Adapter;
+
+import android.view.View;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+
+public class UsuarioAdaptador {
+    public static class ViewHolder extends RecyclerView.ViewHolder{
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
